76123cdb4260959a22d74a899650ef6ba69219c0
ivanlass <ivansky56@gmail.com>
2022-12-12 12:55:20 +0100

    add chakra, husky

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Flex h='100vh' bg='#ededed' justifyContent='center' alignItems='center'>
+      <Text fontSize='6xl' fontWeight='extrabold' color='#dbdb2e'>
+        Hello SafeSys
+      </Text>
+    </Flex>
+  );
+}
+
+export default App;
