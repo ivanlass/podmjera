1a1703a839ed1d2280044c506e43654a2443649b
ivanlass <ivansky56@gmail.com>
2023-12-07 19:14:37 +0100

    add production env

--- src/API/axios.config.js
+++ src/API/axios.config.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
 
-export const API_URL_BASE = 'http://localhost:6060';
-
 // make base url for all axios requests
-axios.defaults.baseURL = API_URL_BASE;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL_BASE;
